fa0032b8e414ff90cf4018b60f448f810eac7f87
firatorhan <f-ratorhan@hotmail.com>
2022-03-21 14:38:30 +0300

    feat: add 'Background Color Changer' project

---
+++ Day_1-10/Day_2/BackgroundColorChanger/js/index.js
@@ -0,0 +1,12 @@
+const button = document.querySelector("button");
+const body = document.querySelector("body");
+const colors = ["blue", "green", "pink", "yellow", "red", "brown", "purple", "orange", "gold", "aqua", "white", "gray", "azure"];
+
+function changeTheBackground() {
+    const colorIndex = parseInt(Math.random()*colors.length)
+    body.style.backgroundColor = colors[colorIndex];
+
+};
+
+body.style.backgroundColor = "purple";
+button.addEventListener("click",changeTheBackground);
